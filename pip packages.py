6f706eb54d8d0a4525762969fd9eb9bd5661b846
Vignesh Allam <57632883+allamvignesh@users.noreply.github.com>
2022-05-17 17:08:04 +0530

    update all python modules

---
+++ pip packages.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+
+subprocess.call("pip install --upgrade pip --user")
+
+packages = [i[:i.find('=')] for i in subprocess.getoutput("pip list --outdated --format=freeze").split('\n')]
+a = input("[GUIDE] Do you want to upgrade all packages: ")
+
+for i in range(len(packages)):
+    print(f"\n[ ({i+1}/{len(packages)})PACKAGE] {packages[i]}")
+    if a.lower() == 'yes' or a.lower() == 'y':
+        subprocess.call(f"pip install -U {packages[i]}")
+    else:
+        inp = input(f"[GUIDE] Do you want to install {packages[i]}: ")
+        if inp.lower() == "yes" or inp.lower() == 'y':
+            subprocess.call(f"pip install -U {packages[i]}")
